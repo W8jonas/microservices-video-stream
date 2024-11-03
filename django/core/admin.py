89f80d3b02ff76475d4b9b775aa500aa2130dfdb
W8jonas <jonashenrique989@gmail.com>
2024-11-03 18:55:11 -0300

    Cria página upload_video html e adicionar url ao path do admin

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.shortcuts import render
+from django.urls import path
+
 from core.models import Video, Tag
 
+
+class VideoAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+			path('<int:id>/upload-video', self.upload_video, name='core_video_create')
+		]
+
+        return custom_urls + urls
+
+    def upload_video(self, request, id):
+        return render(request, 'admin/core/upload_video.html')
+
 # Register your models here.
-admin.site.register(Video)
+admin.site.register(Video, VideoAdmin)
 admin.site.register(Tag)
