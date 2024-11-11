1a0441adf20e92ae711423848ce154d3d5448fb1
W8jonas <jonashenrique989@gmail.com>
2024-11-10 22:26:05 -0300

    Start finalize_upload

--- django/core/services.py
+++ django/core/services.py
@@ -1,5 +1,8 @@
 
 class VideoService:
 
-    def process_upload(self, video_id: int, chunk_index: int, chunk: bytes):
+    def process_upload(self, video_id: int, chunk_index: int, chunk: bytes) -> None:
+        pass
+
+    def finalize_upload(self, video_id: int, total_chunks: int):
         pass
