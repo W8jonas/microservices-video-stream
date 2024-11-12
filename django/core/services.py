3ce5eba2556a1b9ae967227e572dae35b2f0e7d7
W8jonas <jonashenrique989@gmail.com>
2024-11-11 23:44:37 -0300

    Add get_chunk_directory e find_video

--- django/core/services.py
+++ django/core/services.py
@@ -1,5 +1,12 @@
+from core.models import Video, VideoMedia
 
 class VideoService:
+
+    def get_chunk_directory(self, video_id: int) -> str:
+        return f'/tmp/videos/{video_id}'
+
+    def find_video(self, video_id: int) -> Video:
+        return Video.objects.get(id=video_id)
 
     def process_upload(self, video_id: int, chunk_index: int, chunk: bytes) -> None:
         pass
