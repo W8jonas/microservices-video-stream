5e4d48f0949c98a8a30a84f7bcfab96ec0b97ec4
W8jonas <jonashenrique989@gmail.com>
2024-11-19 14:34:29 -0300

    Cria extractNumber

--- proccess_videos/cmd/videoconverter/main.go
+++ proccess_videos/cmd/videoconverter/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"path/filepath"
+	"regexp"
+	"strconv"
+)
+
 func main() {
 	println("Hello world!")
+}
+
+func extractNumber(fileName string) int {
+	re := regexp.MustCompile(`\d+\`)
+	numStr := re.FindString(filepath.Base(fileName))
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return -1
+	}
+	return num
 }
