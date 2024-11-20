35daecbaed84d61d76459a828d78fe13a94bcd0a
W8jonas <jonashenrique989@gmail.com>
2024-11-19 22:21:51 -0300

    add processVideo, extractNumber e mergeChunks para package

--- proccess_videos/internal/converter/task.go
+++ proccess_videos/internal/converter/task.go
@@ -4,6 +4,12 @@ import (
 	"encoding/json"
 	"log/slog"
 	"time"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
 )
 
 type VideoConverter struct {
@@ -22,6 +28,19 @@ func (vc *VideoConverter) Handle(msg []byte) {
 	}
 }
 
+func (vc *VideoConverter) processVideo(task VideoTask) error {
+	mergedFile := filepath.Join(task.Path, "merged.mp4")
+	mpegDashPath := filepath.Join(task.Path, "mpeg-dash")
+
+	slog.Info("Merging chunks", slog.String("path", task.Path))
+	err := vc.mergeChunks(task.Path, mergedFile)
+	if err != nil {
+		vc.logError(*task, "Failed to merge chunks", err)
+		return fmt.Errorf("Failed to merge chunks: %v", err)
+	}
+
+}
+
 func (vc *VideoConverter) logError(task VideoTask, message string, err error) {
 	errorData := map[string]any{
 		"video_id": task.VideoId,
@@ -32,4 +51,45 @@ func (vc *VideoConverter) logError(task VideoTask, message string, err error) {
 	serializedError, _ := json.Marshal(errorData)
 
 	slog.Error("Processing error: ", slog.String("error_details", string(serializedError))
+}
+
+func (vc *VideoConverter) extractNumber(fileName string) int {
+	re := regexp.MustCompile(`\d+`)
+	numStr := re.FindString(filepath.Base(fileName))
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return -1
+	}
+	return num
+}
+
+func (vc *VideoConverter) mergeChunks(inputDir, outputFile string) error {
+	chunks, err := filepath.Glob(filepath.Join(inputDir, "*.chunk"))
+	if err != nil {
+		return fmt.Errorf("Failed to find chunks: %v", err)
+	}
+
+	sort.Slice(chunks, func(i, j int) bool {
+		return vc.extractNumber(chunks[i]) < vc.extractNumber(chunks[j])
+	})
+
+	output, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("Failed to create output file: %v", err)
+	}
+	defer output.Close()
+
+	for _, chunk := range chunks {
+		input, err := os.Open(chunk)
+		if err != nil {
+			return fmt.Errorf("Failed to open chunk: %v", err)
+		}
+
+		_, err = output.ReadFrom(input)
+		if err != nil {
+			return fmt.Errorf("Failed to write chunk %s to merged file: %v", chunk, err)
+		}
+		input.Close()
+	}
+	return nil
 }
