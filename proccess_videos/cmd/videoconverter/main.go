77e5af8af5689cef3a31b953dc07b83193d95569
W8jonas <jonashenrique989@gmail.com>
2024-11-20 20:32:24 -0300

    Refatora main para executar o VideoConverter

--- proccess_videos/cmd/videoconverter/main.go
+++ proccess_videos/cmd/videoconverter/main.go
@@ -1,55 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
-	"path/filepath"
-	"regexp"
-	"sort"
-	"strconv"
+	"videoprocess/internal/converter"
 )
 
 func main() {
-	mergeChunks("mediatest/media/uploads/1", "merged.mp4")
-}
 
-func extractNumber(fileName string) int {
-	re := regexp.MustCompile(`\d+`)
-	numStr := re.FindString(filepath.Base(fileName))
-	num, err := strconv.Atoi(numStr)
-	if err != nil {
-		return -1
-	}
-	return num
-}
+	path := "/media/uploads/1"
 
-func mergeChunks(inputDir, outputFile string) error {
-	chunks, err := filepath.Glob(filepath.Join(inputDir, "*.chunk"))
-	if err != nil {
-		return fmt.Errorf("Failed to find chunks: %v", err)
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		log.Fatalf("Erro ao criar diretório: %v", err)
 	}
 
-	sort.Slice(chunks, func(i, j int) bool {
-		return extractNumber(chunks[i]) < extractNumber(chunks[j])
-	})
-
-	output, err := os.Create(outputFile)
-	if err != nil {
-		return fmt.Errorf("Failed to create output file: %v", err)
-	}
-	defer output.Close()
-
-	for _, chunk := range chunks {
-		input, err := os.Open(chunk)
-		if err != nil {
-			return fmt.Errorf("Failed to open chunk: %v", err)
-		}
-
-		_, err = output.ReadFrom(input)
-		if err != nil {
-			return fmt.Errorf("Failed to write chunk %s to merged file: %v", chunk, err)
-		}
-		input.Close()
-	}
-	return nil
+	vc := converter.NewVideoConverter()
+	vc.Handle([]byte(`{"video_id": 1, "path": "` + path + `"}`))
 }
